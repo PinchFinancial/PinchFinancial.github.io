d976a954c6359ab8f5ccfeb5db7e3441f4eb2af6
Alex Chen <achen831008@gmail.com>
2018-10-31 11:45:46 -0400

    FE-005: Added landing component to home

---
+++ src/modules/Home/Landing.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import Colors from '../../colors'
+
+const styles = {
+  Top: {
+    height: '70vh'
+  },
+  TopMargin: {
+    marginTop: 180
+  },
+  TopHeader: {
+    color: Colors.red,
+    fontSize: 60
+  },
+  TopSubheader: {
+    color: Colors.gray
+  }
+}
+
+class Landing extends Component {
+  render() {
+    return (
+    <Container fluid style={styles.Top}>
+      <div style={styles.TopMargin}>
+        <h1 className="text-center" style={styles.TopHeader}>Renting to Buying</h1>
+        <h3 className="text-center" style={styles.TopSubheader}>We've got everything you need to write your Homestory</h3>
+      </div>
+    </Container>
+    )
+  }
+}
+export default Landing;

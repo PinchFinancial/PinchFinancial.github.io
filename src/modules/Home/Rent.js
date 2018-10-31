570d7df9e06d26a9ccc9c78ba1e3a9a673c42d49
Alex Chen <achen831008@gmail.com>
2018-10-31 11:47:23 -0400

    FE-005: Added Rent component to home

---
+++ src/modules/Home/Rent.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, Button } from 'reactstrap';
+import Colors from '../../colors'
+import RentImage from './img/rent.png'
+
+const styles = {
+  Rent: {
+    minHeight: 500,
+    background: Colors.purple,
+    color: Colors.white
+  },
+  SectionHeader: {
+    fontSize: 80
+  },
+  SectionMargin: {
+    marginTop: 75
+  },
+  PurpleButton: {
+    color: Colors.purple,
+    background: Colors.white,
+    border: '1px solid ' + Colors.purple
+  }
+}
+
+class Rent extends Component {
+  render() {
+    return (
+    <Container fluid style={styles.Rent}>
+        <Row>
+          <Col md={{ size: 4, offset: 1 }}>
+            <div style={styles.SectionMargin}>
+                <h1 style={styles.SectionHeader}>Rent</h1>
+                <p>Whether it’s finding a place, or insuring your rental, we’ve got you covered. We’ve brought together real estate firms and insurance companies to make renting simple. With Pinch you can purchase tenant insurance in less than 5 minutes, no phone calls necessary.</p>
+                <Button size="lg" style={styles.PurpleButton}>BUY INSURANCE</Button>
+            </div>
+          </Col>
+          <Col md={{ size: 7}}>
+            <img src={RentImage} width={'100%'} height={'100%'}></img>
+          </Col>
+        </Row>
+    </Container>
+    )
+  }
+}
+export default Rent;

19ec03d77a034ab413114df6d3be261a158df7ad
Alex Chen <achen831008@gmail.com>
2018-11-08 13:15:39 -0500

    FE-011: refactor contact component to use footer

--- src/modules/Home/Contact.js
+++ src/modules/Home/Contact.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import Colors from '../../colors'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fab)
+import Colors from '../../colors';
+import { Footer } from '../Footer';
 
 const styles = {
     ContactHeader: {
@@ -17,25 +14,6 @@ const styles = {
     },
     SectionMargin: {
         marginTop: 75
-    },
-    SectionBottom: {
-        marginTop: 75,
-        marginBottom: 75
-    },
-    ContactBody: {
-        color: Colors.white,
-        background: Colors.red
-    },
-    link: {
-        color: Colors.white,
-        marginLeft: '5%',
-        marginRight: '5%'
-    },
-    CenterDiv: {
-        display: 'flex',
-        justifyContent: 'center',
-        background: Colors.red,
-        padding: '5%'
     }
 }
 
@@ -51,25 +29,9 @@ class Contact extends Component {
                         </div>
                     </Col>
                 </Row>
-                <Row style={styles.ContactBody}>
-                    <Col md={{ size: 12 }}>
-                        <div style={styles.SectionMargin}>
-                            <p className="text-center">10 Dundas Street E. Toronto, ON M5B 0A1</p>
-                            <a href='mailto:info@pinchfinancial.com'>
-                                <p className="text-center" style={styles.link}>info@pinchfinancial.com</p>
-                            </a>
-                        </div>
-                    </Col>
-                </Row>
             </Container>
-            <div style={styles.CenterDiv}>
-                <a href="https://www.facebook.com/PinchFinancial/" style={styles.link}><FontAwesomeIcon icon={['fab', 'facebook']} size="4x" /></a>
-                <a href="https://www.instagram.com/pinchfinancial/" style={styles.link}><FontAwesomeIcon icon={['fab', 'instagram']} size="4x" /></a>
-                <a href="https://twitter.com/pinchfinancial/" style={styles.link}><FontAwesomeIcon icon={['fab', 'twitter']} size="4x" /></a>
-            </div>
+            <Footer background={Colors.red} fontColor={Colors.white} />
         </div>
-
-
     )
   }
 }

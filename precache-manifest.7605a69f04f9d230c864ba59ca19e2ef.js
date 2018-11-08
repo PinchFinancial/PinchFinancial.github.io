afabd8302f11570575d9132dc2c11e6b34ceaec1
Alex Chen <achen831008@gmail.com>
2018-11-08 15:04:18 -0500

    Updates

---
+++ precache-manifest.7605a69f04f9d230c864ba59ca19e2ef.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6847d63a456558978b25f04ac9eb6199",
+    "url": "/static/media/about.6847d63a.png"
+  },
+  {
+    "revision": "55764e947cf78dc906bf",
+    "url": "/static/css/main.20c69ed8.chunk.css"
+  },
+  {
+    "revision": "91f4275c5cf92315dd049ff2685be939",
+    "url": "/static/media/bg_mobile.91f4275c.jpg"
+  },
+  {
+    "revision": "3f6c8b2f486bde175636",
+    "url": "/static/js/1.3f6c8b2f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39455d1718cb7c4d34b375ea0d292823",
+    "url": "/static/media/ShortLogo.39455d17.png"
+  },
+  {
+    "revision": "22f9b6ddaf6ba8ddc2cdbac0985340bc",
+    "url": "/static/media/rent.22f9b6dd.png"
+  },
+  {
+    "revision": "0728f36e4ddfccfc8dfd72656ca5e42e",
+    "url": "/static/media/buy.0728f36e.png"
+  },
+  {
+    "revision": "55764e947cf78dc906bf",
+    "url": "/static/js/main.55764e94.chunk.js"
+  },
+  {
+    "revision": "32d6a71b36251c0ab9dc6be5ecba4a65",
+    "url": "/static/media/watch.32d6a71b.png"
+  },
+  {
+    "revision": "104eaffa4054ef5a236ab49c93270ba9",
+    "url": "/static/media/computer.104eaffa.png"
+  },
+  {
+    "revision": "55dd2f6d96a3df8e56a8672b8703ee79",
+    "url": "/static/media/bike.55dd2f6d.png"
+  },
+  {
+    "revision": "c821a7b36ee9a6f8ff73252c460ca55f",
+    "url": "/static/media/coverage.c821a7b3.png"
+  },
+  {
+    "revision": "d116d612ff2f56d0d79b6f58fccb5184",
+    "url": "/static/media/motel.d116d612.png"
+  },
+  {
+    "revision": "02c7f06ed2463a4ec5cd680df9415694",
+    "url": "/static/media/bg.02c7f06e.jpg"
+  },
+  {
+    "revision": "3f6c8b2f486bde175636",
+    "url": "/static/css/1.85ca16aa.chunk.css"
+  },
+  {
+    "revision": "d034c25a416b108e4c850e6b53f454f9",
+    "url": "/index.html"
+  }
+];
